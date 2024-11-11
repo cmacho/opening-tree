afaa2c0d7894fc7fb63bdc0d0d543da2afe41d09
Clemens Macho <cle.macho@gmail.com>
2024-11-11 06:15:44 +0100

    Refactor method to util.py

--- explore_openings.py
+++ explore_openings.py
@@ -3,6 +3,7 @@ import chess
 import chess.pgn
 import random
 import pathlib
+from util import read_pgn_files_in_directory
 
 
 def check_exit(user_input):
@@ -60,24 +61,6 @@ def ask_to_enter_anything(prompt, options):
         if user_input == s:
             return options[i]
     return None
-
-
-def read_pgn_files_in_directory(path):
-    """ returns a list of chess.pgn.Game objects, one for each pgn file in path, containing the data from the
-    pgn files.
-
-    Args:
-        path: a pathlib.Path object. The path for the directory from which we want to read pgn files
-    Returns:
-        games_list: (list) list of chess.pgn.Game objects
-    """
-    list_data_dir = list(path.glob("*"))
-    games_list = []
-    for file_path in list_data_dir:
-        with open(file_path) as pgn:
-            game = chess.pgn.read_game(pgn)
-            games_list.append(game)
-    return games_list
 
 
 def main():
